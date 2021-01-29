31bdc4e598401195b5be474aebc6bcb0cb7a19c1
GiangHuyCyris <gianghuy.cyris@gmail.com>
2021-01-29 15:29:13 +0700

    home page UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './share/navbar/navbar.component';
 import { MenuItemsComponent } from './share/navbar/menu-items/menu-items.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './share/header/header.component';
+import { FooterComponent } from './share/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    MenuItemsComponent
+    MenuItemsComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
